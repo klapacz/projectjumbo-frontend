c76aad23d34372c231f9f300da406316d6dc4383
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2021-07-09 09:35:36 +0000

    use router

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <HelloWorld/>
+	<router-view></router-view>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from "vue-router";
 
 </script>
